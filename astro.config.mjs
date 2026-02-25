023dbf4ece35734dc82ab5df2d9c58943e9942f9
Niklas Ekman <nekm@netlight.com>
2026-02-25 18:30:16 +0100

    Add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,15 @@ import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
 export default defineConfig({
 	integrations: [
 		starlight({
+			favicon: '/favicon.svg',
+			head: [{
+				tag: 'link',
+				attrs: {
+					rel: 'icon',
+					href: '/favicon.ico',
+					sizes: '32x32',
+				},
+			}],
 			title: 'mindpad',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mindpad-eu/docs' }],
 			plugins: [
