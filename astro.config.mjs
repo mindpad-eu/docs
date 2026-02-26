ba88696c430b27ea8b1d9e63716fe9b5e5e65d82
Niklas Ekman <nekm@netlight.com>
2026-02-26 22:29:39 +0100

    Add mindpad theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,49 @@ import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
 
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
 export default defineConfig({
 	integrations: [
 		starlight({
 			components: {
 				Footer: './src/components/Footer.astro',
+				SiteTitle: './src/components/SiteTitle.astro',
 			},
 			favicon: '/favicon.svg',
-			head: [{
-				tag: 'link',
-				attrs: {
-					rel: 'icon',
-					href: '/favicon.ico',
-					sizes: '32x32',
+			logo: {
+				replacesTitle: true,
+				dark: './src/assets/logo-dark.svg',
+				light: './src/assets/logo-light.svg',
+			},
+			head: [
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'icon',
+						href: '/favicon.ico',
+						sizes: '32x32',
+					},
 				},
-			}],
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'preconnect',
+						href: 'https://fonts.googleapis.com',
+					},
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'preconnect',
+						href: 'https://fonts.gstatic.com',
+						crossorigin: true,
+					},
+				},
+			],
 			title: 'mindpad',
+			customCss: ['./src/styles/theme.css'],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mindpad-eu/docs' }],
 			plugins: [
 				starlightOpenAPI([{
